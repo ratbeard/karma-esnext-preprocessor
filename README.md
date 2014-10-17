ed551922b09afe52a99eaa160899a25e89548685
Sean Dunn <sedunn@adobe.com>
2014-10-17 09:04:47 -0400

    Initial working version

--- README.md
+++ README.md
@@ -31,7 +31,7 @@ module.exports = function(config) {
 
     esnextPreprocessor: {
       options: {
-        defaultOnly: true
+        arrayComprehensions: false
       }
     }
   });
---
+++ index.js
@@ -0,0 +1,19 @@
+var esnext = require('esnext');
+
+var createEsnextPreprocessor = function(args, config, logger, helper) {
+  config = config || {};
+
+  var log = logger.create('preprocessor.esnext');
+  var options = helper.merge({}, args.options || {}, config.options || {});
+
+  return function(content, file, done) {
+    log.debug('Processing "%s"', file.originalPath);
+    return done(null, esnext.compile(content, options));
+  };
+};
+
+createEsnextPreprocessor.$inject = ['args', 'config.esnextPreprocessor', 'logger', 'helper'];
+
+module.exports = {
+  'preprocessor:esnext': ['factory', createEsnextPreprocessor]
+};
